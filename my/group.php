0a82e0bdd7b9a54271478e58853ce3ceaa729ced
Ayub Ziveh <eyub.zewe@gmail.com>
2025-07-25 02:30:31 +0330

    d

--- my/group.php
+++ my/group.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$telegramToken = '8042806154:AAEfPvKYRfLoHvZ70vIj4PlQ4btqflFRmn4';
-$gapApiKey = 'sk-Srl5P534RBwmzNb0SBqCTGTTa1ZCJDWOoFAqcw3ecsSNiA5e';
+$telegramToken = '';
+$gapApiKey = '';
 
 define('TELEGRAM_API_URL', "https://api.telegram.org/bot$telegramToken/");
 define('GAP_API_URL', 'https://api.gapgpt.app/v1/chat/completions');
